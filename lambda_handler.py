702df401fa833184af3199f14887c554dae1d5be
Arkadiusz Kulpa <arkadiusz.kulpa.91@gmail.com>
2025-05-03 18:06:12 +0100

    updating pipeline

--- lambda_handler.py
+++ lambda_handler.py
@@ -9,7 +9,7 @@ def lambda_handler(event, context):
     try:
         path = event['rawPath']
         method = event['requestContext']['http']['method']
-
+        ##just to get new commit
         if path == "/api" and method == "POST":
             # Parse the request body
             body = json.loads(event.get('body', '{}'))
